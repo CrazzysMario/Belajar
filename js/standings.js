60a68c9b6c879e39905e390741bb0ef2c96d30cf
Ghaza <akmalkay12@gmail.com>
2022-03-13 14:44:21 +0700

    JS Folder

--- js/standings.js
+++ js/standings.js
@@ -1,6 +1,18 @@
 var dataStandings = [
-  ["Heat", "45", "23", ".662", "-", "29-13", "24-8", "21-15", "8-2", "W1"],
-  ["Heat", "45", "23", ".662", "-", "29-13", "24-8", "21-15", "8-2", "W1"],
+  // ["Heat", "45", "23", ".662", "-", "29-13", "24-8", "21-15", "8-2", "W1"],
+  // ["Heat", "45", "23", ".662", "-", "29-13", "24-8", "21-15", "8-2", "W1"],
+  {
+    team: "Heat",
+    win: "45",
+    lose: "23",
+    pct: ".622",
+    gb: "-",
+    conf: "29-13",
+    home: "24-8",
+    away: "21-15",
+    l10: "8-2",
+    strk: "W1",
+  },
 ];
 
 var standItem;
@@ -8,18 +20,18 @@ dataStandings.map((item) => {
   var newTeam = document.createElement("div");
   var standingsList = "";
   newTeam.className = "standings";
-  item.map((secItem, index) => {
-    if(index == 0) {
-      standItem = `<div id="d">${secItem}</div>`;
-    } else if(index == 1) {
-      standItem = `<div id="a">${secItem}</div>`
-    } else if(index == 2) {
-      standItem = `<div id="c">${secItem}</div>`
+  for (secItem in item) {
+    if (secItem == "team") {
+      standItem = `<div id="d">${item[secItem]}</div>`;
+    } else if (secItem == "win") {
+      standItem = `<div id="a">${item[secItem]}</div>`;
+    } else if (secItem == "lose") {
+      standItem = `<div id="c">${item[secItem]}</div>`;
     } else {
-      standItem = `<div>${secItem}</div>`
+      standItem = `<div>${item[secItem]}</div>`;
     }
     standingsList += standItem;
-  });
+  }
   newTeam.innerHTML = standingsList;
   document.getElementById("standings-cards").append(newTeam);
 });
