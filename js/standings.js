9473c70031a20eaa97d1ab372b7293934d1caa69
Ghaza <akmalkay12@gmail.com>
2022-03-20 15:06:19 +0700

    Minor Upgrade

--- js/standings.js
+++ js/standings.js
@@ -1,37 +1,34 @@
-var dataStandings = [
-  // ["Heat", "45", "23", ".662", "-", "29-13", "24-8", "21-15", "8-2", "W1"],
-  // ["Heat", "45", "23", ".662", "-", "29-13", "24-8", "21-15", "8-2", "W1"],
-  {
-    team: "Heat",
-    win: "45",
-    lose: "23",
-    pct: ".622",
-    gb: "-",
-    conf: "29-13",
-    home: "24-8",
-    away: "21-15",
-    l10: "8-2",
-    strk: "W1",
-  },
-];
-
-var standItem;
-dataStandings.map((item) => {
-  var newTeam = document.createElement("div");
-  var standingsList = "";
-  newTeam.className = "standings";
-  for (secItem in item) {
-    if (secItem == "team") {
-      standItem = `<div id="d">${item[secItem]}</div>`;
-    } else if (secItem == "win") {
-      standItem = `<div id="a">${item[secItem]}</div>`;
-    } else if (secItem == "lose") {
-      standItem = `<div id="c">${item[secItem]}</div>`;
-    } else {
-      standItem = `<div>${item[secItem]}</div>`;
-    }
-    standingsList += standItem;
+async function getData(url) {
+  url = '../json/' + url;
+  try {
+    var res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log(error)
+    return null;
   }
-  newTeam.innerHTML = standingsList;
-  document.getElementById("standings-cards").append(newTeam);
-});
+}
+
+async function renderStandings() {
+  var dataStandings = await getData('standings');
+
+  dataStandings.map((item) => {
+    var newTeam = document.createElement("div");
+    var standingsList = "";
+    newTeam.className = "standings";
+    for (secItem in item) {
+      if (secItem == "team") {
+        var standItem = `<div id="d">${item[secItem]}</div>`;
+      } else if (secItem == "win") {
+        var standItem = `<div id="a">${item[secItem]}</div>`;
+      } else if (secItem == "lose") {
+        var standItem = `<div id="c">${item[secItem]}</div>`;
+      } else {
+        var standItem = `<div>${item[secItem]}</div>`;
+      }
+      standingsList += standItem;
+    }
+    newTeam.innerHTML = standingsList;
+    document.getElementById("standings-cards").append(newTeam);
+  });
+}
